194561fb09d320c4ba86b760b633b6be3e056f21
Kelechi <kelechiowuama24@gmail.com>
2022-10-13 11:53:07 -0700

    added doc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+*main - Entry point
+*Description: prints out a string
+*Return: Always 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
