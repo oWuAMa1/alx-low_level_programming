a34301c2ef8d6e943963643f6412ad2a55ba5d57
Kelechi <kelechiowuama24@gmail.com>
2022-10-15 13:32:31 -0700

    modified

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 /**
 *main - entry point
-*Desc.: print combination of two two-digit numbers
-*Return: Always 0
+*Desc.: Prints all possible combinations of two two-digit numbers,
+*        ranging from 0-99, separated by a comma followed by a space.
+*Return: Always 0.
 */
 int main(void)
-{
-	int a;
-	int b;
-	int x;
-	int y;
 
-	for (a = '0'; a <= '9'; a++)
+{
+
+	int a, b;
+
+	for (a = 0; a <= 98; a++)
+
 	{
-		for (b = '0'; b <= '8'; b++)
+
+		for (b = 1; b <= 99; b++)
+
 		{
-			for (x = '0'; x <= '9'; x++)
+			if (a >= b)
 			{
-				for (y = '0'; y <= '9'; y++)
-				{
-					if (a > x || b >= y)
-					{
-						continue;
-					}
-					putchar(a);
-					putchar(b);
-					putchar(' ');
-					putchar(x);
-					putchar(y);
-					if (a == '9' && b == '8' && x == '9' && y == '9')
-					{
-						continue;
-					}
-					putchar(',');
-					putchar(' ');
-				}
+				continue;
 			}
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 98 && b == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
 		}
+
 	}
 
 	putchar('\n');
