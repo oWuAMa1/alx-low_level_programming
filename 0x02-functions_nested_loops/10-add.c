fefa53dab3934d3b9e0b990ade608cbbbab569b7
Kelechi <kelechiowuama24@gmail.com>
2022-10-19 08:39:58 -0700

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add - entry point
+*@x: integer
+*@y: integer
+*Desc.: add two numbers
+*Return: x + y
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*
+*
+*/
+void times_table(void)
+{
+	for (x = 0; x <= 9; x++)
+	{
+		
+	}
+}
