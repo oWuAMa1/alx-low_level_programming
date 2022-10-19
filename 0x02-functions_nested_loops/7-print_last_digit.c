2bca6591872c2763dc2443f81b24474eef47459b
Kelechi <kelechiowuama24@gmail.com>
2022-10-19 07:05:57 -0700

    changed return value

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,9 @@
 *print_last_digit - entry point
 *@n : number
 *Desc: prints the last digit of x
-*Return: Always 0
+*Return: remainder of n
 */
 int print_last_digit(int n)
 {
-	int x = n % 10;
-
-	_putchar(x);
-	return (0);
+	return (n % 10);
 }
