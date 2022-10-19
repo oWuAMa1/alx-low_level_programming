e3b676dc1dde2adb0fe733fdd0edcb98939079ee
Kelechi <kelechiowuama24@gmail.com>
2022-10-19 05:19:39 -0700

    absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_abs - entry point
+*@x: integer
+*Desc: computes absolute value of integer
+*Return: Always 0
+*/
+int _abs(int x)
+{
+	int abs(int x);
+	return (0);
+}
