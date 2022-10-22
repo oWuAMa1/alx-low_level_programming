ee034892dc06432c6e6a5925f7aed4c0e6d24712
Kelechi <kelechiowuama24@gmail.com>
2022-10-22 05:35:02 -0700

    a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,14 +1,25 @@
 #include "main.h"
 /**
-*
-*
+*print_diagonal - entry point
+*@n: number of times
+*Desc: Draws a diagonal line
 */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
-		
+		for (i = 0; i < n; i++)
+		{
+			j = 0;
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
