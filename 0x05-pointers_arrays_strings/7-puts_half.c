4baef1409e22e583f98458165a1e044b27dce13b
Kelechi <kelechiowuama24@gmail.com>
2022-10-25 13:01:57 -0700

    half of str

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+*puts_half - prints half of string
+*@str: string
+*/
+void puts_half(char *str)
+{
+	int i;
+
+	int MAX = strlen(str);
+
+	if (MAX % 2 == 0)
+	{
+		for (i = MAX / 2; i < MAX; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = MAX + 1 / 2; i < MAX; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
