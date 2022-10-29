1ffb324f6d44e9d112df01727ed05f176df7c7a9
Kelechi <kelechiowuama24@gmail.com>
2022-10-28 18:12:42 -0700

    atoi function 2.0

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,6 +9,40 @@
 */
 int _atoi(char *s)
 {
-	int str_to_int = atoi(s);
-	return (str_to_int);
+	int result = 0;
+
+	int sign = 1;
+
+	int i = 0;
+
+	int count_minus = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			count_minus++;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			break;
+		}
+		i++;
+	}
+	if (count_minus % 2 == 1)
+	{
+		sign = -1;
+	}
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + s[i] - '0';
+		}
+		if (s[i + 1] < '0' || s[i + 1] > '9')
+		{
+			break;
+		}
+	}
+	return (sign * result);
 }
