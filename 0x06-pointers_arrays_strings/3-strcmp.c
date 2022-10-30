0fc13248019f3e324f29f28f1400c2b6836f671c
Kelechi <kelechiowuama24@gmail.com>
2022-10-30 07:03:25 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strcmp - compare two strings
+*@s1: string
+*@s2: string
+*Return: result of comparison
+*/
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
