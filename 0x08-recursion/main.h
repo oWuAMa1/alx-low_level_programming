880f5be6026c21adc1b74e85b9b4ef613834af65
Kelechi <kelechiowuama24@gmail.com>
2022-11-08 04:15:51 -0800

    print in reverse

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -58,6 +58,7 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
