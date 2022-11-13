b0534bfbd0ece4cd04cd0c2b89b4e3d7c75643c5
Kelechi <kelechiowuama24@gmail.com>
2022-11-12 19:33:28 -0800

    if palindrom

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,41 +23,22 @@ int compare_str(char *str1, char *str2)
 		return (1);
 	}
 }
-/**
-*strlength - gives the length of a string
-*@len: incremented value to give the length
-*@s: string
-*Return: length of the string
-*/
-int strlength(int len, char *s)
-{
-	if (*s != '\0')
-	{
-		len++;
-		strlength(len, ++s);
-	}
-	else
-	{
-		return (len);
-	}
-}
+
 /**
 *to_nullchar - take pointer to the nullchar
 *@strlen: length of string
 *@s: string
 *Return: pointer to last character before '\0'
 */
-char *to_nullchar(int strlen, char *s)
+char *to_nullchar(char *s)
 {
-	int j = strlen - 1;
-
-	if (j >= 0)
+	if (*s == '\0')
 	{
-		to_nullchar(--strlen, ++s);
+		return (s - 1);
 	}
 	else
 	{
-		return (s - 1);
+		return (to_nullchar(++s));
 	}
 }
 /**
@@ -67,9 +48,7 @@ char *to_nullchar(int strlen, char *s)
 */
 int is_palindrome(char *s)
 {
-	int i = 0, strlen = strlength(i, s);
-	char *str;
-
-	str = to_nullchar(strlen, s);
+	char *str = to_nullchar(s);
 	return (compare_str(s, str));
 }
+
