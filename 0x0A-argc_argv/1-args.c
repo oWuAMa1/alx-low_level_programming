888294d01b95aac7571019a69f0f720c7ed2afb9
Kelechi <kelechiowuama24@gmail.com>
2022-11-17 17:18:09 -0800

    subtracted by 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main - prints the number of arguments in the command line
+*main - prints the number of arguments passed into it excluding itself
 *@argc: argument count
 *@argv:  an array of pointers to each argument
 *Return: Always 0 for success
@@ -9,6 +9,6 @@ int main(int argc, char *argv[])
 {
 	(void)argv;
 
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
