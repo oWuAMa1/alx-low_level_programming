1899b4b6653d36e4e564f96e987114e5e4b93449
Kelechi <kelechiowuama24@gmail.com>
2022-11-17 18:24:29 -0800

    add args of completely integers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - adds positive integers
+*@argc: argument count
+*@argv:  an array of pointers to each argument
+*Return: Always 0 for success
+*/
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]))
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
